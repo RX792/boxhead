0deece4d14361eba91b02ab51435896f12657e3e
Iconer <yoyofa2@hotmail.com>
2022-11-30 13:41:11 +0900

    main 함수 갈아 엎는 중 조심

---
+++ boxhead/boxhead/Main.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <OpenGL.hpp>
+#include <Pipeline.hpp>
+#include <VertexStream.hpp>
+#include <Blobs.hpp>
+#include <Utils.hpp>
+#include <BlobUtils.hpp>
+
+#include "misc.h"
+#include "shader.h"
+#include "input.h"
+#include "box.h"
+#include "zombie.h"
+#include "line.h"
+
+GLvoid Render(GLvoid);
+GLvoid UpdateView(const int w, const int h);
+GLvoid UpdateKeyboard(const unsigned char key, const int x, const int y);
+GLvoid UpdateSpecialKeyboard(const int key, const int x, const int y);
+GLvoid UpdateMouse(const int button, const int state, const int x, const int y);
+GLvoid UpdateMouseMotion(const int x, const int y);
+GLvoid UpdateFrames(const int delta_time);
+
+class Framework
+{
+public:
+	Framework()
+	{
+		
+	}
+	
+	void Awake()
+	{
+		
+	}
+
+	void Start()
+	{
+		
+	}
+
+	void Update(const float& delta_time)
+	{
+		
+	}
+};
+
+float RandomizeColour();
