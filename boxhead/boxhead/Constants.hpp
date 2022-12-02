ad75a15328c33e6bc1dc0a75bab2788a63b44c2c
Iconer <yoyofa2@hotmail.com>
2022-12-02 21:08:25 +0900

    해상도 설정 파일 추가

---
+++ boxhead/boxhead/Constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace constants
+{
+	constexpr int CLIENT_W{ 800 }, CLIENT_H{ 600 };
+	constexpr float CLIENT_RATIO = static_cast<float>(CLIENT_H) / static_cast<float>(CLIENT_W);
+};
