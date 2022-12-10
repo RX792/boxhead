779e5c8f3c8ba58dfdb1a7b3df5f040c252d204d
Iconer <yoyofa2@hotmail.com>
2022-12-10 19:01:52 +0900

    안쓰는 VBO 코드 삭제

--- boxhead/boxhead/GameScene.hpp
+++ boxhead/boxhead/GameScene.hpp
@@ -49,130 +49,18 @@ public:
 
 		orthodoxMatrix = glm::ortho(-400.0f, 400.0f, -300.0f, 300.0f);
 
-
-		constexpr auto axis_color = ogl::Colour{ 0.0f, 0.0f, 0.0f, 1.0f };
-		const ogl::Vertex axis_lines[] =
-		{
-			{ +400.0f, 0.0f, 0.0f, axis_color },
-			{ -400.0f, 0.0f, 0.0f, axis_color },
-			{ 0.0f, +300.0f, 0.0f, axis_color },
-			{ 0.0f, -300.0f, 0.0f, axis_color },
-			{ 0.0f, 0.0f, -400.0f, axis_color },
-			{ 0.0f, 0.0f, -400.0f, axis_color }
-		};
-
-		// 0: ÁÂÇ¥Ãà
-		//myVertexBuffer.PushRaw(axis_lines);
-
-		constexpr auto spatial_c1 = ogl::Colour{ 0.0f, 1.0f, 1.0f, 1.0f };
-		constexpr auto spatial_c2 = ogl::Colour{ 1.0f, 0.0f, 1.0f, 1.0f };
-		constexpr auto spatial_c3 = ogl::Colour{ 1.0f, 1.0f, 0.0f, 1.0f };
-		constexpr auto spatial_c4 = ogl::Colour{ 1.0f, 0.0f, 0.0f, 1.0f };
-		constexpr auto spatial_c5 = ogl::Colour{ 0.0f, 1.0f, 0.0f, 1.0f };
-		constexpr auto spatial_c6 = ogl::Colour{ 0.0f, 0.0f, 1.0f, 1.0f };
-		// °ËÀº»ö
-		constexpr auto spatial_c7 = ogl::Colour{ 0.0f, 0.0f, 0.0f, 1.0f };
-		// Èò»ö
-		constexpr auto spatial_c8 = ogl::Colour{ 1.0f, 1.0f, 1.0f, 1.0f };
-		// ±Ý»ö
-		constexpr auto spatial_c9 = ogl::Colour{ 1.0f, 0.8f, 0.1f, 1.0f };
-
-		constexpr ogl::Quad pt1 = { -0.5f, +0.5f, -0.5f };
-		constexpr ogl::Quad pt2 = { -0.5f, +0.5f, +0.5f };
-		constexpr ogl::Quad pt3 = { +0.5f, +0.5f, +0.5f };
-		constexpr ogl::Quad pt4 = { +0.5f, +0.5f, -0.5f };
-		constexpr ogl::Quad pt5 = { -0.5f, -0.5f, -0.5f };
-		constexpr ogl::Quad pt6 = { -0.5f, -0.5f, +0.5f };
-		constexpr ogl::Quad pt7 = { +0.5f, -0.5f, +0.5f };
-		constexpr ogl::Quad pt8 = { +0.5f, -0.5f, -0.5f };
-
-		constexpr ogl::blob::ColoredPlane each_sides[] =
-		{
-			ogl::blob::plane::Create(pt1, pt2, pt3, pt4, spatial_c1),
-			ogl::blob::plane::Create(pt1, pt5, pt6, pt2, spatial_c2),
-			ogl::blob::plane::Create(pt2, pt6, pt7, pt3, spatial_c3),
-			ogl::blob::plane::Create(pt1, pt4, pt8, pt5, spatial_c9),
-			ogl::blob::plane::Create(pt3, pt7, pt8, pt4, spatial_c5),
-
-			ogl::blob::plane::Create(pt5, pt8, pt7, pt6, spatial_c6)
-		};
-		const auto raw_cube = ogl::blob::cube::Create(each_sides);
-
-		// 1
-		//myVertexBuffer.Push(raw_cube);
-
-		constexpr ogl::Quad head_pt1 = { -0.45f, +0.2f, -0.3f };
-		constexpr ogl::Quad head_pt2 = { -0.45f, +0.2f, +0.3f };
-		constexpr ogl::Quad head_pt3 = { +0.45f, +0.2f, +0.3f };
-		constexpr ogl::Quad head_pt4 = { +0.45f, +0.2f, -0.3f };
-		constexpr ogl::Quad head_pt5 = { -0.45f, -0.2f, -0.3f };
-		constexpr ogl::Quad head_pt6 = { -0.45f, -0.2f, +0.3f };
-		constexpr ogl::Quad head_pt7 = { +0.45f, -0.2f, +0.3f };
-		constexpr ogl::Quad head_pt8 = { +0.45f, -0.2f, -0.3f };
-
-		constexpr ogl::blob::ColoredPlane head_sides[] =
-		{
-			ogl::blob::plane::Create(head_pt1, head_pt2, head_pt3, head_pt4, spatial_c7),
-			ogl::blob::plane::Create(head_pt1, head_pt5, head_pt6, head_pt2, spatial_c7),
-			ogl::blob::plane::Create(head_pt2, head_pt6, head_pt7, head_pt3, spatial_c7),
-			ogl::blob::plane::Create(head_pt1, head_pt4, head_pt8, head_pt5, spatial_c8),
-			ogl::blob::plane::Create(head_pt3, head_pt7, head_pt8, head_pt4, spatial_c7),
-			ogl::blob::plane::Create(head_pt5, head_pt8, head_pt7, head_pt6, spatial_c7)
-		};
-		const auto head_cube = ogl::blob::cube::Create(head_sides);
-
-		// 2
-		//myVertexBuffer.Push(head_cube);
-
-		constexpr ogl::Quad arm_pt1 = { -0.1f, +0.7f, -0.1f };
-		constexpr ogl::Quad arm_pt2 = { -0.1f, +0.7f, +0.1f };
-		constexpr ogl::Quad arm_pt3 = { +0.1f, +0.7f, +0.1f };
-		constexpr ogl::Quad arm_pt4 = { +0.1f, +0.7f, -0.1f };
-		constexpr ogl::Quad arm_pt5 = { -0.1f, -0.7f, -0.1f };
-		constexpr ogl::Quad arm_pt6 = { -0.1f, -0.7f, +0.1f };
-		constexpr ogl::Quad arm_pt7 = { +0.1f, -0.7f, +0.1f };
-		constexpr ogl::Quad arm_pt8 = { +0.1f, -0.7f, -0.1f };
-
-		constexpr ogl::blob::ColoredPlane arms_sides[] =
-		{
-			ogl::blob::plane::Create(arm_pt1, arm_pt2, arm_pt3, arm_pt4, spatial_c9),
-			ogl::blob::plane::Create(arm_pt1, arm_pt5, arm_pt6, arm_pt2, spatial_c9),
-			ogl::blob::plane::Create(arm_pt2, arm_pt6, arm_pt7, arm_pt3, spatial_c9),
-			ogl::blob::plane::Create(arm_pt1, arm_pt4, arm_pt8, arm_pt5, spatial_c8),
-			ogl::blob::plane::Create(arm_pt3, arm_pt7, arm_pt8, arm_pt4, spatial_c9),
-			ogl::blob::plane::Create(arm_pt5, arm_pt8, arm_pt7, arm_pt6, spatial_c9)
-		};
-		const auto arms_cube = ogl::blob::cube::Create(arms_sides);
-
-		// 3
-		//myVertexBuffer.Push(arms_cube);
-
-		constexpr auto floor_c1 = ogl::Colour{ 0.15f, 0.4f, 0.1f, 1.0f };
-		constexpr auto floor_c2 = ogl::Colour{ 0.6f, 0.2f, 0.0f, 1.0f };
-		constexpr auto floor_c3 = ogl::Colour{ 0.0f, 0.6f, 0.0f, 1.0f };
-		constexpr ogl::blob::ColoredPlane floor = ogl::blob::plane::Create
-		(
-			{ -10.0f, -2.0f, -10.0f, floor_c1 },
-			{ -10.0f, -2.0f, +10.0f, floor_c2 },
-			{ +10.0f, -2.0f, +10.0f, floor_c3 },
-			{ +10.0f, -2.0f, -10.0f, floor_c2 }
-		);
-
-		// 4
-		//myVertexBuffer.Push(floor);
-
+		map_manager = new Map_manager;
+		map_manager->create_box(getThis());
 	}
 
-	GameScene* getThis() {
+	GameScene* getThis()
+	{
 		return this;
 	}
 
 	void Start() override
 	{
 		Scene::Start();
-		map_manager = new Map_manager;
-		map_manager->create_box(getThis());
-		
 
 		UpdateClientRect();
 		ResetCamera();
@@ -230,7 +118,7 @@ public:
 			if (!cursorClicked)
 			{
 				ResetCursorPosNow();
-				
+
 				cursorClicked = true;
 			}
 
@@ -338,7 +226,7 @@ private:
 	{
 		GetClientRect(WindowManager::windowHandle, &clientRect);
 	}
-	
+
 	void ResetCamera()
 	{
 		ResetCamera({ 0.0f, 15.0f, -8.0f }, {});
