6a2420f1a55d74684183d1ad96d27a3ef20d19c2
Iconer <yoyofa2@hotmail.com>
2022-12-12 16:33:36 +0900

    플레이어가 각지게 이동함

--- boxhead/boxhead/Player.hpp
+++ boxhead/boxhead/Player.hpp
@@ -35,17 +35,56 @@ public:
 
 	void OnMouse(const int& button, const int& state, const int& x, const int& y)
 	{
-		
+
 	}
 
 	virtual void OnKeyboard(const unsigned char& key, const int& x, const int& y)
 	{
-		
+		const float delta_time = Timer::GetDeltaTime();
+
+		switch (key)
+		{
+			case 'w':
+			case 'W':
+			{
+				const float move_distance_forward = delta_time * 10.0f;
+				MoveForward(move_distance_forward);
+			}
+			break;
+
+			case 'd':
+			case 'D':
+			{
+				const float move_distance_right = delta_time * 10.0f;
+				MoveStrife(move_distance_right);
+			}
+			break;
+
+			case 'a':
+			case 'A':
+			{
+				const float move_distance_left = delta_time * 10.0f;
+				MoveStrife(-move_distance_left);
+			}
+			break;
+
+			case 's':
+			case 'S':
+			{
+				const float move_distance_backward = delta_time * 4.0f;
+				MoveForward(-move_distance_backward);
+			}
+			break;
+
+			default:
+			{}
+			break;
+		}
 	}
 
 	virtual void OnSpecialKey(const int& key, const int& x, const int& y)
 	{
-		
+
 	}
 
 	Entity* Raycast(GameObject obj_list[], const size_t& count, const float& distance, const glm::vec3& direction) const
